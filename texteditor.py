3b19153e58152cf69d104af910e25f808e7aece0
Tushar <tusharpramanick29@gmail.com>
2026-07-14 21:07:47 +0530

    View menu - Wrap Word

--- texteditor.py
+++ texteditor.py
@@ -11,25 +11,48 @@ class TextEditor:
         self.text = tk.Text(self.root, width=80, height=25, wrap='word', font=('Arial', 12), undo=True)
         self.text.pack(fill='both', expand=True)
 
-        # Creating menu bar
+        # Creating Menu bar
         self.menubar = tk.Menu(self.root)
+        
+        # Creating File menu
         self.filemenu = tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label='File', menu=self.filemenu)
+        
+        # Adding Features inside File menu
         self.filemenu.add_command(label='New', command=self.new_file)
         self.filemenu.add_command(label='Open', command=self.open_file)
         self.filemenu.add_command(label='Save', command=self.save_file)
         self.filemenu.add_command(label='Save As', command=self.save_as)
         self.filemenu.add_separator()
         self.filemenu.add_command(label='Quit', command=self.root.quit)
-        self.menubar.add_cascade(label='File', menu=self.filemenu)
+    
+        # Creating View menu
+        self.view_menu = tk.Menu(self.menubar, tearoff=0)
+        self.menubar.add_cascade(label="View", menu=self.view_menu)
+        
+        # Adding Features inside View menu
+        self.word_wrap = tk.BooleanVar()
+        self.word_wrap.set(True)
+        self.view_menu.add_checkbutton(label='Word Wrap', variable=self.word_wrap, command=self.word_wrap_update, onvalue=1, offvalue=0)
+
+        self.view_menu.add_checkbutton(label='Status Bar', variable='check', onvalue=1, offvalue=0)
 
         self.root.config(menu=self.menubar)
         
-        # Creating status bar
+        # Creating Status bar
         self.status_bar = tk.Label(self.root, anchor=tk.SW, text="Ln: 1 | Col: 0", bd='5px')
         self.status_bar.pack(side='bottom', fill='x')
         
         self.text.bind('<KeyRelease>', self.update_status_bar)
         self.text.bind('<ButtonRelease-1>', self.update_status_bar)
+        
+    # Word Wrap update
+    def word_wrap_update(self):
+        if self.word_wrap.get() :
+            self.text.config(wrap='word')
+        else:
+            self.text.config(wrap='none')
+        
         
     # Creating updating status bar
     def update_status_bar(self, event=None):
